c4b00ae4aab8444b9300211a00073e2c9595ffd6
YukikazeSama <51656908+YukikazeSama@users.noreply.github.com>
2021-03-11 14:35:17 +0800

    enemy spawn
    地图修改 添加少量材质 敌人生成

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public Wave[] waves;
+    public Transform START;
+    public float waveRate = 3;
+
+    void Start()
+    {
+        StartCoroutine(SpawnEnemy());
+    }
+    IEnumerator SpawnEnemy()
+    {
+        foreach(Wave wave in waves)
+        {
+            for (int i=0;i<wave.count;i++)
+            {
+                GameObject.Instantiate(wave.enemyPrefab, START.position, Quaternion.identity);
+                if(i!=wave.count-1)
+                {
+                    yield return new WaitForSeconds(wave.rate);
+                }
+            }
+            yield return new WaitForSeconds(waveRate);
+        }
+    }
+
+}
